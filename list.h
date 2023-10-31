67e283e60f0ab37f3d0ff1d65357b2edc1c4c32c
Nathan G <nathan.germany@efrei.net>
2023-10-31 14:06:31 +0100

    Ajout fichiers

--- list.h
+++ list.h
@@ -2,7 +2,15 @@
 // Created by germa on 10/30/2023.
 //
 
+#include <cell.h>
+
 #ifndef AGENDA_MANAGEMENT_PROJECT_LIST_H
 #define AGENDA_MANAGEMENT_PROJECT_LIST_H
+
+typedef struct s_d_list
+{
+    t_d_cell *head_0;
+    t_d_cell *head_1;
+} t_d_list;
 
 #endif //AGENDA_MANAGEMENT_PROJECT_LIST_H
